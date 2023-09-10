fb1cf0544b8488dc8e9e4b4d723518a360a1bb85
Andrés Mares Sánchez <58720028+RocketMares@users.noreply.github.com>
2023-09-10 03:12:26 -0600

    crrec
    crrec

--- src/componentes/MapasSucursales.jsx
+++ src/componentes/MapasSucursales.jsx
@@ -13,7 +13,7 @@ export const MapasSucursales = () => {
                 <div>
                     <h4>Sucursal del Pedregal:</h4>
                 </div>
-                <p>Av. Tetiz n 292 col Pedregal de San Nicolás 3 sección C.P 14100, <br /> Tlalpan, CDMX.</p>
+                <p>Av. Tetiz n 292 col Pedregal de San Nicolás 3 sección <br />C.P 14100,  Tlalpan, CDMX.</p>
                 <div>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.850627472154!2d-99.23778722770261!3d19.288860942089983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdffaa9177cf79%3A0x8c66ceafc3d76f18!2sAv.%20Tetiz%20292%2C%20Pedregal%20de%20San%20Nicol%C3%A1s%202da%20Secc%2C%20Tlalpan%2C%2014100%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1694334690747!5m2!1ses-419!2smx" 
                     width="600" 
@@ -27,7 +27,7 @@ export const MapasSucursales = () => {
                 <div>
                     <h4>Sucursal Lomas de Padierna:</h4>
                 </div>
-                <p>Av. Yucalpeten n 25 col Lomas de padierna C. P. 14240. <br /> Tlalpan, CDMX.</p>
+                <p>Av. Yucalpeten n 25 col Lomas de padierna  <br />C. P. 14240. Tlalpan, CDMX.</p>
                 <div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.730297300223!2d-99.22480792555673!3d19.294091381955905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdffb1f1d00df1%3A0x605d9f88a523cc11!2sYucalpeten%2025%2C%20Lomas%20de%20Padierna%2C%20Tlalpan%2C%2014240%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1694334933340!5m2!1ses-419!2smx" 
                 width="600" 
@@ -41,7 +41,7 @@ export const MapasSucursales = () => {
                 <div>
                     <h4>Sucursal Iztapalapa:</h4>
                 </div>
-                <p>C. Sur 111-A 649 local 3 col. Sector Popular, C. P. 09060, <br /> Iztapalapa, CDMX</p>
+                <p>C. Sur 111-A 649 local 3 col. Sector Popular,<br /> C. P. 09060,  Iztapalapa, CDMX</p>
                 <div>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.05185181563!2d-99.11712592555455!3d19.366907381898493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fe6da601056f%3A0x588f8283901cac6d!2sSur%20111-A%20649-local%203%2C%20Sector%20Popular%2C%20Popular%2C%20Iztapalapa%2C%2009230%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1694334988775!5m2!1ses-419!2smx" 
                 width="600" 
