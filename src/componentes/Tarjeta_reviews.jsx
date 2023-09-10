06b86ee7d62fc724176e122b4dd369b744132db6
Andrés Mares Sánchez <58720028+RocketMares@users.noreply.github.com>
2023-09-10 03:49:07 -0600

    ingreso
    imagen doctores

--- src/componentes/Tarjeta_reviews.jsx
+++ src/componentes/Tarjeta_reviews.jsx
@@ -6,14 +6,14 @@ export const Tarjeta_reviews = ({Nombre,Especialidad,servics}) => {
         <div className='reviews__review' >
             <h4 className='review__title' >{Nombre}</h4>
             <span className='review'>
-                <img className='review__img' src="" alt="Doctor" />
+                <img className='review__img' src="./img/doctor.png" alt="Doctor" />
             </span>
             <p className='review__description'>
                 {Especialidad}
             </p>
             <div className='review__autor' >
                 <div className='autor__container-img' >
-                    <img className='autor__img' src="" alt="foto_doctor" />
+                 
                 </div>
                 <p className='review__description'>
                 {servics}
--- src/paginas/Especialistas.jsx
+++ src/paginas/Especialistas.jsx
@@ -14,17 +14,20 @@ export const Especialistas = () => {
                     Especialistas
                 </h4>
                 <h2 className="reviews__title">¿Quiénes son?</h2>
-                <p className="reviews__description" >
-                    Nuestro personal cuenta experiencia de vanguardia y de alta de manda en su campo 
+                 <p className="reviews__description" >
+                 Somos jóvenes Profesionistas enfocados en los servicios de salud que buscan brindar a otros humanos calidez, empatía, y confianza, brindando un servicio de calidad y eficaz adaptándonos a las necesidades de nuestros clientes por medio de un trato personalizado. Comprendemos que “cada cabeza es un mundo” por lo que, no podremos invadir tu mundo, pero si hacer una transformación para una mejor calidad de vida. 
                 </p>
             </header>
-           {
+            <div className="reviews__grid-container" >
+            {
             datos.map(dato =>{
                 return(
                     <Tarjeta_reviews key={dato.id} Nombre={dato.nombreEspecialista} Especialidad ={dato.Especialidad} servics = {dato.Corriente} />
                 )
             })
            }
+            </div>
+        
         </div>
     </section>
       
